181c5c7c9c37d22413e2560b047400adb3c8467f
Gart2357 <gart@outlook.jp>
2021-10-10 14:40:14 +0900

    post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import subprocess
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,70 +8,35 @@ app = Flask(__name__)
 def help():
     return '/tex for math mode and /texp for text mode'
 
-@app.route('/tex/<s>', methods=['GET'])
-def render_tex(s=None):
+@app.route('/', methods=['POST'])
+def render_tex():
 
-    r = subprocess.run(
-        ['sudo', '-u', 'tex', '-i', 'python', '/home/tex/tex/tex.py'],
-        input=s,
-        stdout=subprocess.PIPE,
-        text=True,
-    )
-    if r.returncode == 0:
-        result = {
-            'status': 0,
-            'result': r.stdout,
-        }
-    elif r.returncode == 1:
-        result = {
-            'status': 1,
-            'error': r.stdout,
-        }
-    elif r.returncode == 2:
-        result = {
-            'status': 2,
-        }
+    if request.json.get('code') is None:
+        return None, 400
+    if request.json.get('type') == 'png':
+        if not request.json.get('plain'):
+            r = subprocess.run(
+                ['sudo', '-u', 'tex', '-i', 'python', '/home/tex/tex/tex.py'],
+                input=request.json.get('code'),
+                stdout=subprocess.PIPE,
+                text=True,
+            )
+        else:
+            r = subprocess.run(
+                ['sudo', '-u', 'tex', '-i', 'python', '/home/tex/tex/tex.py', '-p'],
+                input=request.json.get('code'),
+                stdout=subprocess.PIPE,
+                text=True,
+            )
+    elif request.json.get('type') == 'pdf':
+        r = subprocess.run(
+            ['sudo', '-u', 'tex', '-i', 'python', '/home/tex/tex/texpdf.py'],
+            input=request.json.get('code'),
+            stdout=subprocess.PIPE,
+            text=True,
+        )
     else:
-        return None, 500
-    return jsonify(result)
-
-@app.route('/texp/<s>', methods=['GET'])
-def render_texp(s=None):
-
-    r = subprocess.run(
-        ['sudo', '-u', 'tex', '-i', 'python', '/home/tex/tex/tex.py', '-p'],
-        input=s,
-        stdout=subprocess.PIPE,
-        text=True,
-    )
-    if r.returncode == 0:
-        result = {
-            'status': 0,
-            'result': r.stdout,
-        }
-    elif r.returncode == 1:
-        result = {
-            'status': 1,
-            'error': r.stdout,
-        }
-    elif r.returncode == 2:
-        result = {
-            'status': 2,
-        }
-    else:
-        return None, 500
-    return jsonify(result)
-
-
-@app.route('/texpdf/<s>', methods=['GET'])
-def render_texpdf(s=None):
-
-    r = subprocess.run(
-        ['sudo', '-u', 'tex', '-i', 'python', '/home/tex/tex/texpdf.py'],
-        input=s,
-        stdout=subprocess.PIPE,
-        text=True,
-    )
+        return None, 400
     if r.returncode == 0:
         result = {
             'status': 0,
